09e649da5eb971d2c6199b67042265b150d01687
Nikita <Niksipeykin@mail.ru>
2021-02-12 01:36:14 +0300

    Create java file lesson4

---
+++ lesson4/src/ru/geekbrains/NSipeykin/lesson4/Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.NSipeykin.lesson4;
+
+public class Main {
+
+//TODO:
+// 1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+//TODO:
+// 2. На серверной стороне сетевого чата реализовать управление потоками через ExecutorService***
+
+    public static void main(String[] args) {
+	    Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+        Thread thread3 = new Thread();
+	
+    }
+}
