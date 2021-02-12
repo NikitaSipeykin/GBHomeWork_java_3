3b9123c510ff12c98165c3a87d8395458eaf6a83
Nikita <Niksipeykin@mail.ru>
2021-02-13 00:25:13 +0300

    Task1 was completed

--- lesson4/src/ru/geekbrains/NSipeykin/lesson4/Main.java
+++ lesson4/src/ru/geekbrains/NSipeykin/lesson4/Main.java
@@ -4,13 +4,38 @@ public class Main {
 
 //TODO:
 // 1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
-//TODO:
-// 2. На серверной стороне сетевого чата реализовать управление потоками через ExecutorService***
+
+    static Object monitor = new Object();
+    static volatile int currentNum = 1;
+    static final int num = 5;
 
     public static void main(String[] args) {
-	    Thread thread1 = new Thread();
-        Thread thread2 = new Thread();
-        Thread thread3 = new Thread();
-	
+        printCharacter(1,'A');
+        printCharacter(2,'B');
+        printCharacter(3,'C');
+    }
+
+    private static void printCharacter(int monitoringNumber, char character){
+        new Thread(()->{
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (monitor){
+                        while (currentNum != monitoringNumber){
+                            monitor.wait();
+                        }
+                        System.out.print(character);
+                        if (monitoringNumber == 3){
+                            System.out.println(); //сделал перенос строки для наглядности
+                            currentNum = 1;
+                        }else {
+                            currentNum = monitoringNumber + 1;
+                        }
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
